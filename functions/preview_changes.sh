dd8cbedaa00b1063501950d684fe562dc4ff0d66
marshki <marshall@morethantomorrow.org>
2019-02-03 10:08:52 -0500

    add preview function

---
+++ functions/preview_changes.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+
+file_type=txt
+find_string="long_and_winding"
+replace_string="yellow_brick" 
+
+preview_changes() {
+  printf "%s\n" "Generating preview..."
+ 
+  for file in *$file_type; do 
+    printf "%s %s \n" "$file" "-->" "${file/$find_string/$replace_string}" 
+  done
+} 
+ 
+preview_changes
