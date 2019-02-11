19e8c4c39b0299daf29104b9f0cd1bedc4ae7d85
marshki <marshall@morethantomorrow.org>
2019-02-11 11:19:08 -0500

    add menu

---
+++ functions/show_menu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+# On-screen menu.
+
+function show_menu() {
+    date
+    printf "%s\\n" "------------------------------"
+    printf "%s\\n" "  BULKY: Main Menu            "
+    printf "%s\\n" "------------------------------"
+        printf "%s\\n" "  1. Rename files by string"
+        printf "%s\\n" "  2. Rename files by type"
+        printf "%s\\n" "  3. Exit"
+}
+
+show_menu
