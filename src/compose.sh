eab6cb4c51cad93a2d6e06368760b14e39370e2d
marshki <marshall@morethantomorrow.org>
2019-02-12 13:09:45 -0500

    update EOF

--- src/compose.sh
+++ src/compose.sh
@@ -157,13 +157,13 @@ main() {
 BULKY: A shell script to rename files en masse. 
 
 BULKY searches for and replaces the first occurrence of a string in all files  
-that begin with a search term (by string), e.g.:  
+that BEGIN with a search term (by string), e.g.:  
 
 pink_white_blue_00.csv --> red_white_blue_00.csv
 pink_white_blue_00.html --> red_white_blue_00.html 
 pink_white_blue_00.txt --> red_white_blue_00.txt 
 
-or all files ending with an extension (by type), e.g.: 
+or all files with an extension (by type), e.g.: 
 
 pink_white_blue_00.txt --> red_white_blue_00.txt  
 pink_white_blue_01.txt --> red_white_blue_01.txt 
