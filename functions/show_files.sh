3bbede1c338ff1a8dbeb2b5490124de963f7e9d8
marshki <marshall@morethantomorrow.org>
2019-02-16 10:27:16 -0500

    add suggestion for alternative approach

--- functions/show_files.sh
+++ functions/show_files.sh
@@ -3,6 +3,9 @@
 # ls column output, placing / after a filename that is a directory
 # grep to return non-directories (files only)
 
+# shellcheck complains about ls to grep pipe
+# find . -maxdepth 1 -type f |cut 
+
 show_files() { 
   printf "%s\n" "Files in current directory: " 
   sleep 1.5 
