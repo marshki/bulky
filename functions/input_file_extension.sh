42e2a78bc5ab8c66529da78e9bd6631d2971bd60
marshki <marshall@morethantomorrow.org>
2019-02-17 10:13:35 -0500

    add -r to read

---
+++ functions/input_file_extension.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+# Prompt to define file_type.  
+# If present, strip "." then reassign output to file_type 
+
+input_file_type() { 
+  printf "%s\n" "File extensions look like this: csv, json, txt." 
+
+  read -rp "Enter the extension of the files to be modified: " file_type
+
+  file_type=${file_type//.}
+  printf "%s\n" "$file_type" 
+ 
+} 
+
+input_file_type
