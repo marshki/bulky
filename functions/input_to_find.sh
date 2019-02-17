3e30ff55f35145dbcc6142289b2ce204c22f6356
marshki <marshall@morethantomorrow.org>
2019-02-17 09:52:22 -0500

    add r option to read && double-quote find_string

--- functions/input_to_find.sh
+++ functions/input_to_find.sh
@@ -3,9 +3,9 @@
 # Retrieve & assign user input. 
 
 input_to_find() { 
-  read -p "Enter a string: " find_string 
+  read -rp "Enter a string: " find_string 
 
-  printf "%s\n" $find_string
+  printf "%s\n" "$find_string"
 
 } 
 
