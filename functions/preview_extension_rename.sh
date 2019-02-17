c99435bda83dfdcecf712fb29ad8e24b24301d12
marshki <marshall@morethantomorrow.org>
2019-02-17 10:40:34 -0500

    add

---
+++ functions/preview_extension_rename.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+file_ext="txt"
+find_string="long_winding"
+replace_string="yellow_brick" 
+
+preview_changes() {
+  printf "%s\n" "Generating preview..."
+ 
+  for file in *$file_ext; do 
+    printf "%s %s %s\n" "$file" "-->" "${file/$find_string/$replace_string}" 
+  done
+} 
+ 
+preview_changes
