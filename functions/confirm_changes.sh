734039d1466eaa5d8d22dce8a02e8d2192a86026
marshki <marshall@morethantomorrow.org>
2019-04-07 09:35:37 -0400

    add return to menu option

--- functions/confirm_changes.sh
+++ functions/confirm_changes.sh
@@ -1,19 +1,24 @@
 #!/bin/bash 
 
-# Prompt user for confirmation; exit if not "yes"
+# Prompt user for confirmation; exit if not "yes" or "menu"
 # Pipe reply to tr to convert to lower case . 
-# Even though we ask for "YES" we will accept, e.g. yes, YeS, yeS. 
+# Even though we ask for "UPPERCASE" we will accept, 
+# e.g. upper, UpPeRcAsE, UPPERCASE.
 
 confirm_changes() { 
-  read -rp "Replace: 'this' with: 'that'(YES/NO)? " answer 
+  read -rp "Replace: 'this' with: 'that'(YES/NO/MENU)? " answer 
 
   answer=$(printf "%s" "$answer"| tr '[:upper:]' '[:lower:]')
 
   if [[ "$answer" = "yes" ]]; then
       printf "%s\n" "Continuing..."
-  else
-      printf "%s\n" "Exiting." 
+
+  elif [[ "$answer" = "no" ]]; then 
+      printf "%s\n" "Exiting..." 
       exit 1
+
+  else 
+      printf "%s\n" "Returning to main menu..."
   fi 
 }
 
