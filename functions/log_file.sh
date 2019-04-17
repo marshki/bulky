7280fa8c94a24b0d34615f8f9089a1d76feca825
marshki <marshall@morethantomorrow.org>
2019-04-17 09:09:47 -0400

    add functions and testing

--- functions/log_file.sh
+++ functions/log_file.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 # Custom log file
 
-TIMESTAMP=$(date "+DATE: %Y-%m-%d%nTIME: %H:%M:%S")
+# TIMESTAMP=$(date "+DATE: %Y-%m-%d%nTIME: %H:%M:%S")
+# printf "%s\n" "$TIMESTAMP"
 
-printf "%s\n" "$TIMESTAMP"
+LOGFILE=bulky.sh.log
+RETAIN_NUM_LINES=10
+
+logsetup () {
+  TMP=$(tail -n $RETAIN_NUM_LINES $LOGFILE 2>/dev/null) && printf "%s" "${TMP}" > $LOGFILE
+  exec > >(tee -a $LOGFILE)
+  exec 2>&1
+}
+
+log () {
+    printf "%s" "[$(date --rfc-3339=seconds)]: $*"
+}
+
+logsetup
+
+log hello this is a log 
