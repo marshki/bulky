c416ec29238941584bb4a2e3c5f5d205ef7493a1
marshki <marshall@morethantomorrow.org>
2019-04-17 18:37:43 -0400

    add comments

--- functions/log_file.sh
+++ functions/log_file.sh
@@ -4,9 +4,12 @@
 LOGFILE=bulky.sh.log
 RETAIN_NUM_LINES=10
 
-# add comments here
+# log_setup: output num of lines from "RETAIN" and send to /dev/null
+#          : print output of variable to "LOGFILE" 
+#          : within process append output to file (do not overwrite) 
+#          : within process redirect standard error to standard out 
 
-logsetup () {
+log_setup () {
   TMP=$(tail -n $RETAIN_NUM_LINES $LOGFILE 2>/dev/null) && printf "%s\n" "${TMP}" > $LOGFILE
   exec > >(tee -a $LOGFILE)
   exec 2>&1
@@ -16,17 +19,23 @@ log () {
     printf "%s\n" "[$(date)]: $*"
 }
 
-logsetup
+log_setup
 
-log bleh
-log hello this is a log
-log hello this is a log, too
-log hello this is a log, three
-log hello this is a log, four
-log hello this is a log, five
-log bleh, bleh
-log hello this is a log, six
-log hello this is a log, seven
-log hello this is a log, eight
-log hello this is a log, nine
-log hello this is a log, ten
+log larry 
+log moe 
+log curly
+log shemp
+log curly joe
+
+#log bleh
+#log hello this is a log
+#log hello this is a log, too
+#log hello this is a log, three
+#log hello this is a log, four
+#log hello this is a log, five
+#log bleh, bleh
+#log hello this is a log, six
+#log hello this is a log, seven
+#log hello this is a log, eight
+#log hello this is a log, nine
+#log hello this is a log, ten
