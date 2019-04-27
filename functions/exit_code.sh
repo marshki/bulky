da4fef6d544e1c75c496697760a9f925b2225d96
marshki <marshall@morethantomorrow.org>
2019-04-27 08:34:25 -0400

    update comments

--- functions/exit_code.sh
+++ functions/exit_code.sh
@@ -1,14 +1,14 @@
 #!/bin/bash 
-# Exit code status
+# Get exit code status for last called function
 
 exit_code() { 
 
   if [ $? -eq 0 ]; then 
     printf "%s\n" "Success."
-    #exit 0
+    exit 0
   else
     printf "%s\n" "Failure." >&2
-    #exit 1
+    exit 1
   fi
 }
 
