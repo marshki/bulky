3a805618e0ee4b056402015b201c1b057252be1c
marshki <marshall@morethantomorrow.org>
2019-04-27 08:40:05 -0400

    rm "function" in front of read

--- functions/read_input.sh
+++ functions/read_input.sh
@@ -2,7 +2,7 @@
 # Get input via keyboard and make a decision using case...esac. 
 # read -r (do not allow backslashes to escape any characters) -p (prompt)  
 
-function read_input() {
+read_input() {
     local c
     read -rp "Enter your choice [ 1-3 ]:  " c
     case $c in
